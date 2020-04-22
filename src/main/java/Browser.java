d230186fb17b847b99d70da6583924c9025b495d
Ajith-oo7 <51732096+Ajith-oo7@users.noreply.github.com>
2020-04-22 16:40:15 +0530

    web commit

--- src/main/java/Browser.java
+++ src/main/java/Browser.java
@@ -17,104 +17,104 @@ import java.net.*;
 import java.io.*;
 
 public class Browser {
-    private JFrame frame;
-    private JPanel panelTop;
-    private JEditorPane editor;
-    private JScrollPane scroll;
-    private JTextField field;
-    private JButton button;
-    private URL url;
+        private JFrame frame;
+        private JPanel panelTop;
+        private JEditorPane editor;
+        private JScrollPane scroll;
+        private JTextField field;
+        private JButton button;
+        private URL url;
 
-    public Browser(String title) {
-        initComponents();
+        public Browser(String title) {
+                initComponents();
 
-        //set the title of the frame
-        frame.setTitle(title);
+                //set the title of the frame
+                frame.setTitle(title);
 
-        //set the default cloe op of the jframe
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                //set the default cloe op of the jframe
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //set size of frame
-        frame.setSize(800,600);
+                //set size of frame
+                frame.setSize(800,600);
 
-        //add jpanel to north of jframe
-        frame.add(BorderLayout.NORTH, panelTop);
+                //add jpanel to north of jframe
+                frame.add(BorderLayout.NORTH, panelTop);
 
-        //add textfield and navigation button to jpanel.
-        panelTop.add(field);
-        panelTop.add(button);
+                //add textfield and navigation button to jpanel.
+                panelTop.add(field);
+                panelTop.add(button);
 
-        //add scroll pane to jframe center
-        frame.add(BorderLayout.CENTER, scroll);
+                //add scroll pane to jframe center
+                frame.add(BorderLayout.CENTER, scroll);
 
-        
-        //set the frame visible
-        frame.setVisible(true);
-    }//end Browser() constructor
 
-    private void initComponents() {
-        //create the JFrame
-        frame = new JFrame();
+                //set the frame visible
+                frame.setVisible(true);
+        }//end Browser() constructor
 
-        //create the JPanel used to hold the text field and button.
-        panelTop = new JPanel();
-        
-        //set the url
-        try {
-            url = new URL("http://www.javagaming.org");
-        }
-        catch(MalformedURLException mue) {
-            JOptionPane.showMessageDialog(null,mue);
-        }
-        
-        //create the JEditorPane
-        try {
-            editor = new JEditorPane(url);
-            
-            //set the editor pane to false.
-            editor.setEditable(false);
-        }
-        catch(IOException ioe) {
-            JOptionPane.showMessageDialog(null,ioe);
-        }
-        
-        //create the scroll pane and add the JEditorPane to it.
-        scroll = new JScrollPane(editor, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        private void initComponents() {
+                //create the JFrame
+                frame = new JFrame();
 
-        //create the JTextField
-        field = new JTextField();
+                //create the JPanel used to hold the text field and button.
+                panelTop = new JPanel();
 
-        //set the JTextField text to the url.
-        //we're not doing this on the event dispatch thread, so we need to use
-        //SwingUtilities.
-        SwingUtilities.invokeLater(new Runnable() {
-           public void run() {
-               field.setText(url.toString());
-           }
-        });
-
-        //create the button for chanign pages.
-        button = new JButton("Go");
-        
-        //add action listener to the button
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+                //set the url
                 try {
-                    editor.setPage(field.getText());
+                        url = new URL("http://www.javagaming.org");
+                }
+                catch(MalformedURLException mue) {
+                        JOptionPane.showMessageDialog(null,mue);
+                }
+
+                //create the JEditorPane
+                try {
+                        editor = new JEditorPane(url);
+
+                        //set the editor pane to false.
+                        editor.setEditable(false);
                 }
                 catch(IOException ioe) {
-                    JOptionPane.showMessageDialog(null, ioe);
+                        JOptionPane.showMessageDialog(null,ioe);
                 }
-            }
-        });
-    }//end initComponents()
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new Browser("Simple web browser");
-            }
-        });
-    }//end main method.
+                //create the scroll pane and add the JEditorPane to it.
+                scroll = new JScrollPane(editor, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                        JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+                //create the JTextField
+                field = new JTextField();
+
+                //set the JTextField text to the url.
+                //we're not doing this on the event dispatch thread, so we need to use
+                //SwingUtilities.
+                SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                                field.setText(url.toString());
+                        }
+                });
+
+                //create the button for chanign pages.
+                button = new JButton("Go");
+
+                //add action listener to the button
+                button.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                                try {
+                                        editor.setPage(field.getText());
+                                }
+                                catch(IOException ioe) {
+                                        JOptionPane.showMessageDialog(null, ioe);
+                                }
+                        }
+                });
+        }//end initComponents()
+
+        public static void main(String[] args) {
+                SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                                new Browser("Simple web browser");
+                        }
+                });
+        }//end main method.
 }//end Browser class
